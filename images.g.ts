6cf13cfd4aff4985f86a536907d78e9fc698833d
sethpcodeninjas <79381106+sethpcodeninjas@users.noreply.github.com>
2021-03-06 13:42:57 -0500

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -3,7 +3,182 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
-
+            case "image1":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image2":return img`
+..............ffffff....
+.............fffffffff..
+............fffffffffff.
+............ffffffffffff
+...........fffffffffffff
+...........ffffffffffff.
+...........fffeeefffffff
+...........fee44fbe44eff
+............feccf14d4eef
+.............fcccc4eeef.
+.............fe444eccf..
+.............ccc22eccf..
+.............cdc22fee...
+............cddc4444f...
+...........cddcfffff....
+..........cddc..fff.....
+..........cdc...........
+..........cc............
+........................
+........................
+........................
+........................
+........................
+........................
+`;
+            case "image3":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image4":return img`
+..............ffffff....
+.............fffffffff..
+............fffffffffff.
+............ffffffffffff
+...........fffffffffffff
+...........ffffffffffff.
+...........fffeeefffffff
+...........fee44fbe44eff
+............feccf14d4eef
+.............fcccc4eeef.
+.............fe444eccf..
+.............ccc22eccf..
+.............cdc22fee...
+............cddc4444f...
+...........cddcfffff....
+..........cddc..fff.....
+..........cdc...........
+..........cc............
+........................
+........................
+........................
+........................
+........................
+........................
+`;
+            case "image5":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image6":return img`
+..............ffffff....
+.............fffffffff..
+............fffffffffff.
+............ffffffffffff
+...........fffffffffffff
+...........ffffffffffff.
+...........fffeeefffffff
+...........fee44fbe44eff
+............feccf14d4eef
+.............fcccc4eeef.
+.............fe444eccf..
+.............ccc22eccf..
+.............cdc22fee...
+............cddc4444f...
+...........cddcfffff....
+..........cddc..fff.....
+..........cdc...........
+..........cc............
+........................
+........................
+........................
+........................
+........................
+........................
+`;
+            case "image7":return img`
+..............ffffff....
+.............fffffffff..
+............fffffffffff.
+............ffffffffffff
+...........fffffffffffff
+...........ffffffffffff.
+...........fffeeefffffff
+...........fee44fbe44eff
+............feccf14d4eef
+.............fcccc4eeef.
+.............fe444eccf..
+.............ccc22eccf..
+.............cdc22fee...
+............cddc4444f...
+...........cddcfffff....
+..........cddc..fff.....
+..........cdc...........
+..........cc............
+........................
+........................
+........................
+........................
+........................
+........................
+`;
+            case "image8":return img`
+. . . . f f f f f . . . . . . . 
+. . . f e e e e e f . . . . . . 
+. . f d d d d e e e f . . . . . 
+. c d f d d f d e e f f . . . . 
+. c d f d d f d e e d d f . . . 
+c d e e d d d d e e b d c . . . 
+c d d d d c d d e e b d c . f f 
+c c c c c d d d e e f c . f e f 
+. f d d d d d e e f f . . f e f 
+. . f f f f f e e e e f . f e f 
+. . . . f e e e e e e e f f e f 
+. . . f e f f e f e e e e f f . 
+. . . f e f f e f e e e e f . . 
+. . . f d b f d b f f e f . . . 
+. . . f d d c d d b b d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,65 +1,69 @@
-// We made a new SpriteKind.Ninja
 namespace SpriteKind {
     export const Ninja = SpriteKind.create()
 }
-
-
-function makeEnemy () {
-	let evilNinja = sprites.create(img`
-     ..............ffffff....
-     .............fffffffff..
-     ............fffffffffff.
-     ............ffffffffffff
-     ...........fffffffffffff
-     ...........ffffffffffff.
-     ...........fffeeefffffff
-     ...........fee44fbe44eff
-     ............feccf14d4eef
-     .............fcccc4eeef.
-     .............fe444eccf..
-     .............ccc22eccf..
-     .............cdc22fee...
-     ............cddc4444f...
-     ...........cddcfffff....
-     ..........cddc..fff.....
-     ..........cdc...........
-     ..........cc............
-     ........................
-     ........................
-     ........................
-     ........................
-     ........................
-     ........................
- `, SpriteKind.Enemy)
- evilNinja.vx = 40
- evilNinja.vy = 60
- evilNinja.y = 0
- evilNinja.x = randint(0, 160)
- evilNinja.setBounceOnWall(true)
-}
-
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Ninja, function (sprite, otherSprite) {
-	
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate)
+    scene.cameraShake(4, 500)
+    sprite.say(randomSayings._pickRandom(), 500)
 })
-
+function makeEnemy () {
+    info.changeScoreBy(1)
+    evilNinja = sprites.create(img`
+        ..............ffffff....
+        .............fffffffff..
+        ............fffffffffff.
+        ............ffffffffffff
+        ...........fffffffffffff
+        ...........ffffffffffff.
+        ...........fffeeefffffff
+        ...........fee44fbe44eff
+        ............feccf14d4eef
+        .............fcccc4eeef.
+        .............fe444eccf..
+        .............ccc22eccf..
+        .............cdc22fee...
+        ............cddc4444f...
+        ...........cddcfffff....
+        ..........cddc..fff.....
+        ..........cdc...........
+        ..........cc............
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        ........................
+        `, SpriteKind.Ninja)
+    evilNinja.vx = 40
+    evilNinja.vy = 60
+    evilNinja.y = 0
+    evilNinja.x = randint(0, 160)
+    evilNinja.setBounceOnWall(true)
+}
+let evilNinja: Sprite = null
+let randomSayings: string[] = []
+randomSayings = ["YEET", "RASENGAN", "CHIDORI", "BRUH", "RASENGAN"]
+info.setLife(10)
+info.setScore(0)
 let monkeyLeftImg = img`
-    . . . . f f f f f . . . . . . .
-    . . . f e e e e e f . . . . . .
-    . . f d d d d e e e f . . . . .
-    . c d f d d f d e e f f . . . .
-    . c d f d d f d e e d d f . . .
-    c d e e d d d d e e b d c . . .
-    c d d d d c d d e e b d c . f f
-    c c c c c d d d e e f c . f e f
-    . f d d d d d e e f f . . f e f
-    . . f f f f f e e e e f . f e f
-    . . . . f e e e e e e e f f e f
-    . . . f e f f e f e e e e f f .
-    . . . f e f f e f e e e e f . .
-    . . . f d b f d b f f e f . . .
-    . . . f d d c d d b b d f . . .
-    . . . . f f f f f f f f f . . .
-`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f . . . . . 
+    . c d f d d f d e e f f . . . . 
+    . c d f d d f d e e d d f . . . 
+    c d e e d d d d e e b d c . . . 
+    c d d d d c d d e e b d c . f f 
+    c c c c c d d d e e f c . f e f 
+    . f d d d d d e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f e f 
+    . . . f e f f e f e e e e f f . 
+    . . . f e f f e f e e e e f . . 
+    . . . f d b f d b f f e f . . . 
+    . . . f d d c d d b b d f . . . 
+    . . . . f f f f f f f f f . . . 
+    `
 let monkeyRightImg = monkeyLeftImg.clone()
 monkeyRightImg.flipX()
 let monkey = sprites.create(monkeyLeftImg, SpriteKind.Player)
@@ -68,3 +72,6 @@ monkey.y = 110
 monkey.setStayInScreen(true)
 makeEnemy()
 scene.setBackgroundColor(1)
+game.onUpdateInterval(2000, function () {
+    makeEnemy()
+})
